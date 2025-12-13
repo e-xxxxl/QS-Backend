867a385a0f7a72bfaaccbe301668c3e267a6f48d
emmanuel <eajejohnson@gmail.com>
2025-12-13 19:34:17 +0100

    fix: Update reset password URL to use production frontend URL

--- controllers/authController.js
+++ controllers/authController.js
@@ -721,7 +721,7 @@ exports.forgotPassword = async (req, res) => {
     await user.save();
 
     // Create reset URL
-    const resetUrl = `${process.env.FRONTEND_URL || 'http://localhost:5173'}/reset-password/${resetToken}`;
+    const resetUrl = `${process.env.FRONTEND_URL || 'https://quickship.africa'}/reset-password/${resetToken}`;
 
     // Send reset email using Resend
     try {
